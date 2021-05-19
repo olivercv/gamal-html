1f93d7442409bb9348daa3fae581389aa27c1ea2
Oliver Camacho <oliver_c_v@hotmail.com>
2021-05-18 22:43:18 -0400

    arreglos 18 may

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,7 +2,82 @@
     "use strict";
 
     
+    if ($(".instafeed-gallery-feed").length) {
+        var userFeed = new Instafeed({
+            get: "user",
+            userId: 6443571543,
+            accessToken: "6443571543.3a81a9f.5fa5cc39d0314deba22a8f148881852d",
+            resolution: "standard_resolution",
+            useHttp: "true",
+            limit: 10,
+            template: 
+              // '<a href="{{image}}">'+
+                '<div class="img-featured-container">'+
+                  '<div class="img-backdrop"></div>'+
+                  '<div class="description-container">'+
+                    // '<p class="caption">{{caption}}</p>'+
+                    '<span class="likes"><i class="icon ion-heart"></i> {{likes}}</span>'+
+                    '<span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span>'+
+                  '</div>'+
+                  '<img src="{{image}}" class="img-responsive">'+
+                '</div>',
+              // '</a>'+
+            target: "instafeed-gallery-feed",
+            after: function() {
+              // disable button if no more results to load
+              if (!this.hasNext()) {
+                btnInstafeedLoad.setAttribute('disabled', 'disabled');
+              }
+              
+              var owl = $(".owl-carousel"),
+                  owlSlideSpeed = 300;
+          
+              // init owl    
+              $(document).ready(function(){
+                owl.owlCarousel({
+                  // navContainer: '.owl-nav-custom',
+                  // dotsContainer: '.owl-dots-custom',
+                  margin:10,
+                  loop:true,
+                  margin:10,
+                  nav:true,
+                  responsive:{
+                    0:{
+                      items:1
+                    },
+                    200:{
+                      items:2
+                    },
+                    400:{
+                      items:3
+                    },
+                    768:{
+                      items:5
+                    }
+                  }
+                });
+              });
+          
+              // keyboard controls
+              $(document.documentElement).keydown(function(event) {
+                if (event.keyCode == 37) {
+                  owl.trigger('prev.owl.carousel', [owlSlideSpeed]);
+                }
+                else if (event.keyCode == 39) {
+                  owl.trigger('next.owl.carousel', [owlSlideSpeed]);
+                }
+              });
+            }
+          });
+          
+          userFeed.run();
+          
+          var btnInstafeedLoad = document.getElementById("btn-instafeed-load");
+          btnInstafeedLoad.addEventListener("click", function() {
+            userFeed.next()
+          });
 
+    }
     
     
     // Testimonial One Carousel
