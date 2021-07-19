397608bc156b0dcaa7ebfc08ac0a27c45c5cfc4d
Oliver Camacho <oliver_c_v@hotmail.com>
2021-07-18 22:18:21 -0400

    18 julio

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -13,7 +13,7 @@
             max: 9, // A number between 1 and 25 of photos to show. Default: 9
             autoplay: true, // Set autoplay video: true/false. Default: false
             complete: function () { // A callback function to execute after the display of the photos.
-                console.log('completed');
+                // console.log('completed');
             }
         });
        
@@ -823,7 +823,12 @@ function projectMasonaryLayout() {
         })(jQuery);
 
         $('#myModal').modal('show');
-        // alert('deberia salir');
+
+
+        $('#myModal').on('hidden.bs.modal', function (e) {
+            jQuery("#myModal iframe").attr("src", jQuery("#myModal iframe").attr("src"));
+          })
+
     });
     
     
